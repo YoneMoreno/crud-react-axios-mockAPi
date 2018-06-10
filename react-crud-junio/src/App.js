0da229ec3c36f9e3da7af2a6c2d5ce996fe55427
Yone <yone.moreno101@alu.ulpgc.es>
2018-06-10 16:28:14 +0100

    We have created an unorderer list with list items and a heading to show that we can include bootstrap into a react app

--- react-crud-junio/src/App.js
+++ react-crud-junio/src/App.js
@@ -1,21 +1,26 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <h1 className="App-title">Welcome to React</h1>
+                </header>
+                <div className="container">
+                    <h2 className="text-center p-4">TASKS list!</h2>
+                    <ul className="list-group">
+                        <li className="list-group-item">1</li>
+                        <li className="list-group-item">2</li>
+                        <li className="list-group-item">3</li>
+                    </ul>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
